f38c8c648a74f43a7eb93756dbc7b1b7d1bab693
joelsondeveloper <115474776+joelsondeveloper@users.noreply.github.com>
2025-03-27 22:30:07 -0300

    .

--- cursoLogicaV2/function/area.js
+++ cursoLogicaV2/function/area.js
@@ -10,5 +10,11 @@ function calcularAreaTriangulo(l, a) {
   }
   return (l * a) / 2;
 }
+function calcularAreaCirculo(r) {
+  if (typeof r !== "number") {
+    throw new Error("calcularAreaCirculo: R precisam ser numeros");
+  }
+  return Math.PI * (r ** 2);
+}
 
 console.log(calcularAreaRetangulo(4, 3)); // 15
