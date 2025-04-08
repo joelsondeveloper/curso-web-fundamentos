73cc8ff44f01e461b998b311471e3dbe1ef1ac9a
JoelsonDeveloper <vicentejoelson80@gmail.com>
2025-04-08 14:34:01 -0300

    .

---
+++ cursoLogicaV2/cursoJavascriptV2/arrays/array03.js
@@ -0,0 +1,7 @@
+let arr1 = [1, 2, 3];
+let arr2 = [5, 6, 7];
+
+console.log(arr1.toString())
+console.log(arr1.join(" - "))
+let arr3 = arr1.concat(arr2, 4, 89, 67, ["ola"], ["mundo"])
+console.log(arr3)
