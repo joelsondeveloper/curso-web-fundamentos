b7ee82b7d07326f9701edc78dc668498ca459b54
JoelsonDeveloper <vicentejoelson80@gmail.com>
2025-04-30 23:12:33 -0300

    .

--- cursoLogicaV2/cursoJavascriptV2/objetos/constructor.js
+++ cursoLogicaV2/cursoJavascriptV2/objetos/constructor.js
@@ -3,18 +3,22 @@ function Task(name) {
   this.createdAt = new Date();
   this.updatedAt = null;
   this.changeName = function (newName) {
-    _name = newName;
-    this.updatedAt = new Date();
+    if (newName){
+      _name = newName;
+      this.updatedAt = new Date();
+    }
   };
   this.getName = function () {
     return _name;
   }
 }
 
-const task1 = new Task("minha tarefa");
-task1.changeName("minha tarefa alterada");
+// const task1 = new Task("minha tarefa");
+// task1.changeName("minha tarefa alterada");
 
-const task2 = new Task("minha segunda tarefa");
+const task2 = Task("minha segunda tarefa");
 
-console.log(task1);
+// console.log(task1);
+// console.log(task1.getName());
 console.log(task2);
+console.log(createdAt);
---
+++ cursoLogicaV2/cursoJavascriptV2/strings/introducao.js
@@ -0,0 +1,8 @@
+let str1 = "minha string bacana";
+// console.log(str1.replace("minha", "sua"));
+
+// console.log(str1.indexOf("g"))
+// console.log(str1.includes("bacana"));
+
+console.log(str1.slice(0, 5));
+console.log(str1.substring(0, 5));
