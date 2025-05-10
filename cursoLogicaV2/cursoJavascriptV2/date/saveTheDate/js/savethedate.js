7f523ecea0a048f6d44e2b349b7a9f0f597bb0c8
JoelsonDeveloper <vicentejoelson80@gmail.com>
2025-05-09 22:46:09 -0300

    .

--- cursoLogicaV2/cursoJavascriptV2/date/saveTheDate/js/savethedate.js
+++ cursoLogicaV2/cursoJavascriptV2/date/saveTheDate/js/savethedate.js
@@ -1,42 +1,52 @@
-;(function () {
-    const dateEventDom = document.querySelector('.hero-content span').textContent.trim();
-    const dateEvent = getDate(dateEventDom);
+(function () {
+  const dateEventDom = document
+    .querySelector(".hero-content span")
+    .textContent.trim();
+  const dateEvent = getDate(dateEventDom);
+  const ONE_MINUTE = 60 * 1000;
+  const ONE_HOUR = 60 * ONE_MINUTE;
+  const ONE_DAY = 24 * ONE_HOUR;
 
+  let daysLeft;
+  let hoursLeft;
+  let minutesLeft;
+  let secondsLeft;
+
+  setInterval(() => {
+    updateDate();
+    addLeftTime(daysLeft, hoursLeft, minutesLeft, secondsLeft);
+  }, 1000);
+
+  const p = document.createElement("p");
+  document.querySelector(".hero-content").appendChild(p);
+
+  function addLeftTime(d, h, m, s) {
+    p.textContent = `contagem regressiva: ${d} dias ${h} horas ${m} minutos e ${s} segundos`;
+  }
+
+  function getDate(str) {
+    const [date, hour] = str.split(" ");
+    const [day, month, year] = date.split("/");
+    const [h, m] = hour.split("H");
+    return new Date(year, month - 1, day, h, m);
+  }
+
+  function updateDate() {
     const today = new Date();
 
     let left = dateEvent.getTime() - today.getTime();
     console.log(left);
 
-    const ONE_MINUTE = 60 * 1000;
-    const ONE_HOUR = 60 * ONE_MINUTE;
-    const ONE_DAY = 24 * ONE_HOUR;
-    
-    const daysLeft = parseInt(left / ONE_DAY);
+    daysLeft = parseInt(left / ONE_DAY);
     left -= daysLeft * ONE_DAY;
 
-    const hoursLeft = parseInt(left / ONE_HOUR);
+    hoursLeft = parseInt(left / ONE_HOUR);
     left -= hoursLeft * ONE_HOUR;
 
-    const minutesLeft = parseInt(left / ONE_MINUTE);
+    minutesLeft = parseInt(left / ONE_MINUTE);
     left -= minutesLeft * ONE_MINUTE;
 
-    const secondsLeft = parseInt(left / 1000);
+    secondsLeft = parseInt(left / 1000);
     console.log(daysLeft, hoursLeft, minutesLeft, secondsLeft);
-
-    function addLeftTime(d, h, m, s) {
-        const p = document.createElement('p');
-        p.textContent = `contagem regressiva: ${d} dias ${h} horas ${m} minutos e ${s} segundos`;
-        document.querySelector('.hero-content').appendChild(p);
-    }
-    addLeftTime(daysLeft, hoursLeft, minutesLeft, secondsLeft);
-
-
-
-    function getDate(str) {
-        const [date, hour] = str.split(' ');
-        const [day, month, year] = date.split('/');
-        const [h, m] = hour.split('H');
-        return new Date(year, month - 1, day, h, m);
-    }
-
-})()
+  }
+})();
