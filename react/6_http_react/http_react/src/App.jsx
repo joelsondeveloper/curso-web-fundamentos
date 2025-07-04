e111682d785f25f84667067f07f64a8245d84a9c
JoelsonDeveloper <vicentejoelson80@gmail.com>
2025-07-04 10:30:53 -0300

    .

--- react/6_http_react/http_react/src/App.jsx
+++ react/6_http_react/http_react/src/App.jsx
@@ -1,35 +1,104 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+import { useFetch } from "./hooks/useFetch";
+
+const url = "http://localhost:3000/products";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // 1 - resgatando dados
+  const [product, setProducts] = useState([]);
+
+  // 4 - custom hooks
+
+  const { data: items, httpConfig, loading, error } = useFetch(url);
+
+  // useEffect(() => {
+  //   async function getData() {
+  //     const res = await fetch(url);
+
+  //     const data = await res.json();
+
+  //     setProducts(data);
+  //   }
+
+  //   getData();
+  // }, []);
+
+  // 2 - envio de dados
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const product = {
+      name,
+      price,
+    };
+
+    // 5 - refatorando
+    httpConfig(product, "POST")
+
+
+    // const res = await fetch(url, {
+    //   method: "post",
+    //   headers: {
+    //     "content-type": "application/json",
+    //   },
+    //   body: JSON.stringify(product),
+    // });
+
+    // 3 - carregamento dinamico
+
+    // const addedProduct = await res.json();
+
+    // setProducts((prevProducts) => [...prevProducts, addedProduct]);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <h1>HTTP em React</h1>
+      {loading && <p>Carregando...</p>}
+      {/* 7 - tratando erros */}
+      {error && <p>{error}</p>}
+      {/* 1 - resgate de dados  */}
+      <ul>
+        {items &&
+          items.map((product) => (
+            <li key={product.id}>
+              {product.name} - {product.price}
+            </li>
+          ))}
+      </ul>
+      {/* 2 - enviando dados  */}
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            <span>Nome:</span>
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+          <label>
+            <span>Preço:</span>
+            <input
+              type="text"
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+            />
+          </label>
+          {/* <input type="submit" value="Enviar" /> */}
+          {/* 6 - loading post  */}
+          {loading && <input type="submit" disabled value="Aguarde..."/>}
+          {!loading && <input type="submit" value="Criar"/>}
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
