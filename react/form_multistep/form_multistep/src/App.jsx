7da5663456794b170f91cd656b43bb1dd630effd
JoelsonDeveloper <vicentejoelson80@gmail.com>
2025-07-07 21:38:48 -0300

    .

---
+++ react/form_multistep/form_multistep/src/App.jsx
@@ -0,0 +1,67 @@
+// components
+import { GrFormNext, GrFormPrevious } from "react-icons/gr";
+import { FiSend } from "react-icons/fi";
+import UserForm from "./components/UserForm";
+import ReviewForm from "./components/ReviewForm";
+import Thanks from "./components/Thanks";
+
+// hooks
+import { useForm } from "./hooks/useForm";
+
+import "./App.css";
+import Steps from "./components/Steps";
+
+function App() {
+  const formComponents = [<UserForm />, <ReviewForm />, <Thanks />];
+
+  const { currentStep, currentComponent, changeStep, isLastStep, isFirstStep } =
+    useForm(formComponents);
+
+  return (
+    <div className="app">
+      <div className="header">
+        <h2>Deixe sua avaliação</h2>
+        <p>
+          estamos felizes com a sua compra, utilize o formulariovabaixo para
+          avaliar o produto
+        </p>
+      </div>
+      <div className="form-container">
+        <Steps currentStep={currentStep}/>
+        <form
+          onSubmit={(e) => {
+            changeStep(currentStep + 1, e);
+          }}
+        >
+          <div className="inputs-container">{currentComponent}</div>
+          <div className="actions">
+            {!isFirstStep && (
+              <button
+                type="button"
+                onClick={() => {
+                  changeStep(currentStep - 1);
+                }}
+              >
+                <GrFormPrevious />
+                <span>Voltar</span>
+              </button>
+            )}
+            {!isLastStep ? (
+              <button type="submit">
+                <span>Avançar</span>
+                <GrFormNext />
+              </button>
+            ) : (
+              <button type="button">
+                <span>Enviar</span>
+                <FiSend />
+              </button>
+            )}
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ react/form_multistep/form_multistep/src/components/ReviewForm.jsx
@@ -0,0 +1,43 @@
+import {
+  BsFillEmojiHeartEyesFill,
+  BsFillEmojiSmileFill,
+  BsFillEmojiNeutralFill,
+  BsFillEmojiFrownFill,
+} from "react-icons/bs";
+
+import "./ReviewForm.css"
+
+const ReviewForm = () => {
+  return (
+    <div className="review-form">
+      <div className="form-control score-container">
+        <label className="radio-container">
+          <input type="radio" value="unsatisfied" name="review" required />
+          <BsFillEmojiFrownFill />
+          <p>Insatifeito</p>
+        </label>
+        <label className="radio-container">
+          <input type="radio" value="neutral" name="review" required />
+          <BsFillEmojiNeutralFill />
+          <p>Poderia ser melhor</p>
+        </label>
+        <label className="radio-container">
+          <input type="radio" value="satisfied" name="review" required />
+          <BsFillEmojiSmileFill />
+          <p>Satifeito</p>
+        </label>
+        <label className="radio-container">
+          <input type="radio" value="very_satisfied" name="review" required />
+          <BsFillEmojiHeartEyesFill />
+          <p>Muito satisfeito</p>
+        </label>
+      </div>
+      <div className="form-control">
+        <label htmlFor="comment">Comentario:</label>
+        <textarea name="comment" id="comment" placeholder="Conte como foi sua experiencia com o produto" required></textarea>
+      </div>
+    </div>
+  );
+};
+
+export default ReviewForm;
