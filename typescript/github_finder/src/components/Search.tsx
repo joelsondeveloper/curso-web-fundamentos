b5e82e03a2e76233285a15e41866abc7a47fd523
JoelsonDeveloper <vicentejoelson80@gmail.com>
2025-09-02 16:30:10 -0300

    .

--- typescript/github_finder/src/components/Search.tsx
+++ typescript/github_finder/src/components/Search.tsx
@@ -2,20 +2,29 @@ type SearchProps = {
     loadUser: (userName: string) => Promise<void>
 }
 
-import { useState } from "react"
+import { useState, KeyboardEvent } from "react"
 
 import { BsSearch } from "react-icons/bs"
+
+import classes from './Search.module.css'
 
 const Search = ({ loadUser }: SearchProps) => {
 
   const [userName, setUserName] = useState('')
 
+  const handleKeyDown = (e: KeyboardEvent) => {
+    if (e.key === 'Enter') {
+      loadUser(userName)
+    }
+  }
+
   return (
-    <div>
+    <div className={classes.search}>
       <h2>Busque por um usuário:</h2>
       <p>Conheça seus melhores repositórios</p>
-      <div>
-        <input type="text" placeholder="Digite o nome do usuário" onChange={(e) => setUserName(e.target.value)} value={userName} />
+      <div className={classes.search_container}>
+        <input type="text" placeholder="Digite o nome do usuário" onChange={(e) => setUserName(e.target.value)} value={userName}
+        onKeyDown={handleKeyDown} />
         <button onClick={() => loadUser(userName)}>
           <BsSearch />
         </button>
---
+++ typescript/github_finder/src/components/User.tsx
@@ -0,0 +1,11 @@
+import { UserProps } from "../types/user"
+
+const User = ({login, avatar_url, location, followers, following}) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default User
